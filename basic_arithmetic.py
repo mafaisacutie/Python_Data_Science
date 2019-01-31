cb68628613d0ca3a49390b82eca3e904b966ba5c
mafaisacutie <46110812+mafaisacutie@users.noreply.github.com>
2019-01-31 15:40:58 -0500

    Create basic_arithmetic.py

---
+++ basic_arithmetic.py
@@ -0,0 +1,51 @@
+# //int
+# 1
+# returns 1
+#
+# //float
+# 1
+# returns 1.0
+#
+# //muliplication
+# 2*5
+# returns 10
+#
+# //division
+# 10/2
+# returns 5
+#
+# //addition
+# 1+1
+# returns 2
+#
+# //subtraction
+# 2-1
+# returns 1
+#
+# //exponent
+# 2**4
+# returns 16
+#
+# //Order of operations applies
+# 2+5*5-4
+# returns 23
+#
+# //to specify order, use parenthesis
+# (2+5)*(5-4)
+# returns 7
+#
+# //moduls, returns remainder of the dividing numbers
+# 8%2
+# returns 0 // no remainder left
+#
+# 5%2
+# returns 1 // there is a remainder of 1
+#
+# 8%2
+# returns 0 // no remainder
+
+def main():
+    x = 2
+    print(4%x)
+    #returns 0, no remainder
+main()
